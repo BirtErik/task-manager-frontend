64838854c320109c1248d44031538ff7667ce2af
BirtErik <erikbj3@gmail.com>
2023-10-02 08:48:22 +0200

    Add logout method to navbar/Remove unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserTasksListComponent } from './components/user/user-tasks/list/user-tasks-list.component';
 
 const routes: Routes = [
   {
--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { Router } from '@angular/router';
+import { LoginService } from 'src/app/common/services/login.service';
 
 @Component({
   selector: 'app-navbar',
@@ -13,15 +14,21 @@ export class NavbarComponent implements OnInit {
   activeEditButton: boolean = false;
   enableEdit: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private readonly loginService: LoginService
+  ) {}
 
   ngOnInit(): void {
     this.items = [
       { label: 'Home', routerLink: '/home' },
       { label: 'Tasks', routerLink: '/user' },
-      { label: 'Logout', routerLink: '' },
+      { label: 'Logout', routerLink: '', command: () => this.onLogoutClick() },
     ];
+  }
 
-    //this.activeItem = this.items[0];
+  onLogoutClick() {
+    this.loginService.logout();
+    this.router.navigate(['/home']);
   }
 }
